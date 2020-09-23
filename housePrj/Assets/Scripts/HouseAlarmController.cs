bb625624c9ed4296719eb9b47652bfcea3fba72f
etyatte <etyatte@greet-go.com>
2020-09-24 00:36:46 +0300

    Commit of whole project

---
+++ housePrj/Assets/Scripts/HouseAlarmController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseAlarmController : MonoBehaviour
+{
+    // flash light animator boolean property name
+    private const string IsThiefInsideHouseProperty = "IsThiefInsideHouse";
+
+    [SerializeField] private Collider ThiefCollider;
+    [SerializeField] private Animator AlarmLampAnimator;
+    [SerializeField] private AudioSource AlarmSound;
+    [SerializeField] private float AlarmSoundAdjustSpeed = 100f;
+
+    private bool _isThiefInsideHouse;
+    private float _currentAlarmVolume = 0; // current alarm volume in percent (0% - 100%)
+
+
+    private void Update()
+    {
+        if (_isThiefInsideHouse)
+        {
+            // start alaram sound
+            if (!AlarmSound.isPlaying)
+                AlarmSound.Play();
+
+            // adjusting alarm sound volume to max
+            if (AlarmSound.volume != 1.0f)
+            {
+                _currentAlarmVolume = Mathf.Min(100f, _currentAlarmVolume + AlarmSoundAdjustSpeed * Time.deltaTime);
+                AlarmSound.volume = _currentAlarmVolume / 100;
+            }
+        }
+        else
+        {
+            // adjusting alarm sound volume to min
+            if (AlarmSound.volume != 0.0f)
+            {
+                _currentAlarmVolume = Mathf.Max(0f, _currentAlarmVolume - AlarmSoundAdjustSpeed * Time.deltaTime);
+                AlarmSound.volume = _currentAlarmVolume / 100;
+            }
+
+            // stop alaram sound
+            if (AlarmSound.isPlaying && _currentAlarmVolume == 0.0f)
+                AlarmSound.Stop();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // start flash light animation if thief enter house
+        if(other == ThiefCollider)
+            AlarmLampAnimator.SetBool(
+                IsThiefInsideHouseProperty, _isThiefInsideHouse = true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // stop flash light animation if thief exit house
+        if (other == ThiefCollider)
+            AlarmLampAnimator.SetBool(
+                IsThiefInsideHouseProperty, _isThiefInsideHouse = false);
+    }
+}
---
+++ housePrj/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Dictionary<KeyCode, Action> Actions { get; } // keyboard keys to actions bindings
+    private Rigidbody _rigidbody; // thief rigitbody
+
+    private KeyCode? _currentAction = null; // current action key
+
+    [SerializeField] private float Speed = 5f; // thief speed
+
+    public PlayerController()
+    {
+        // actions initialization
+        Actions = new Dictionary<KeyCode, Action>()
+        {
+            [KeyCode.A] = () => _rigidbody.velocity = Vector3.left * Speed,
+            [KeyCode.D] = () => _rigidbody.velocity = Vector3.right * Speed,
+        };
+    }
+
+    private void Awake()
+    {
+        // fields initialization
+        _rigidbody = transform.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        bool found = false; // is key found during checking
+
+        // checking every key stored in Actions dictionary for pressing
+        foreach (KeyCode key in Actions.Keys)
+        {
+            if(Input.GetKey(key))
+            {
+                found = true;
+
+                // if current action the same as found action - exit checking
+                if (_currentAction == key)
+                    break;
+
+                Actions[key](); // execute key action
+                _currentAction = key; // set current key as current action
+                break;
+            }
+        }
+
+        // if user press no key from Actions dictionary - current action should be empty
+        if (!found)
+            _currentAction = null;
+
+        // if no current action - stop player
+        if (!_currentAction.HasValue)
+            _rigidbody.velocity = Vector3.zero;
+    }
+
+    private void OnGUI()
+    {
+        //
+        // drawing "Thief" text above player
+        //
+
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(_rigidbody.transform.position);
+
+        GUIStyle textStyle = new GUIStyle()
+        {
+            fontSize = 16,
+            active = new GUIStyleState() { textColor = Color.red },
+            normal = new GUIStyleState() { textColor = Color.red },
+        };
+
+        GUI.Label(new Rect(screenPos.x - 20, Screen.height - (screenPos.y + 60), 100, 50), "Thief", textStyle);
+    }
+
+
+}
